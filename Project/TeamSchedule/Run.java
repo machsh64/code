eedc27d30bc710040ed45737ba68c10a1211f6d7
machsh <3256772458@qq.com>
2022-09-14 21:25:32 +0800

    Work3

---
+++ Project/TeamSchedule/Run.java
@@ -0,0 +1,17 @@
+package Project.TeamSchedule;
+
+import Project.TeamSchedule.view.TeamView;
+
+/**
+ * @program: code
+ * @author: Ren
+ * @create: 2022-09-14 21:22
+ * @description:   开发团队模块
+ **/
+public class Run {
+    public static void main(String[] args) {
+        TeamView vie = new TeamView();
+
+        vie.enterMainMean();
+    }
+}
